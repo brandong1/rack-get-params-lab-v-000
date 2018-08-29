04e5b03d5fd468bf615f877a24e0e9942cbd524e
Brandon Green <brandon@brandonleeproductions.com>
2018-08-29 00:48:44 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -18,7 +18,12 @@ class Application
     elsif req.path.match(/cart/)
         @@cart.each do |item|
           resp.write "#{item}\n"
+        else 
+          resp.write "Your cart is empty"
         end
+      elsif req.path.match(/add/)
+        search_term = req.params["item"]
+        resp.write handle_search(search_term)
     else
       resp.write "Path Not Found"
     end
